c284f71b7b020368f7b22b58e6faa0ba551ddefc
Stuart Carruthers <Stuart Carruthers>
2016-07-02 19:22:19 +0100

    First working prototype

--- TwitchResubBot/Program.cs
+++ TwitchResubBot/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using ChatSharp.Events;
+using log4net;
+using log4net.Config;
+using TwitchBot;
 
 namespace TwitchResubBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            XmlConfigurator.Configure();
+            var log = LogManager.GetLogger("TwitchResubBot");
+            var settingsReader = new AppSettingsReader();
+            var resubLogPath = settingsReader.GetValue("ResubOutputFilepath", typeof(string)).ToString();
+            var username = settingsReader.GetValue("Username", typeof(string)).ToString();
+            var token = settingsReader.GetValue("OAuthToken", typeof(string)).ToString();
+            var channel = settingsReader.GetValue("Channel", typeof(string)).ToString();
+            if (string.IsNullOrEmpty(resubLogPath)) throw new ArgumentException("Path to resub log file was not found.  Please check the App.config.");
+            if (string.IsNullOrEmpty(username)) throw new ArgumentException("Username was not found.  Please check the App.config.");
+            if (string.IsNullOrEmpty(token)) throw new ArgumentException("OAuth token was not found.  Please check the App.config.");
+            if (string.IsNullOrEmpty(channel)) throw new ArgumentException("Channel name was not found.  Please check the App.config.");
+
+            var resubLogger = new TextLogger(resubLogPath);
+            log.Info("Application started.");
+
+            var bot = new TwitchClient("irc.chat.twitch.tv", username, token, channel);
+            bot.BotListening += (sender, eventArgs) => { log.Info("Bot is now listening for resubs."); };
+            bot.UserSubbed += (sender, eventArgs) =>
+            {
+                log.Info($"{eventArgs.Username} has just subscribed!");
+                resubLogger.WriteLine($"{eventArgs.Username} (new subscriber)");
+            };
+            bot.UserResubbed += (sender, eventArgs) =>
+            {
+                log.Info($"{eventArgs.Username} subscribed for {eventArgs.Months} months! - {eventArgs.Message}");
+                resubLogger.WriteLine($"{eventArgs.Username} ({eventArgs.Months} months)");
+            };
+
+            bot.Connect();
+
+            var exit = false;
+            while (!exit)
+            {
+                var input = Console.ReadKey();
+                if (input.KeyChar.Equals('q')) { exit = true; }
+            }
         }
     }
 }
---
+++ TwitchResubBot/TextLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace TwitchResubBot
+{
+    public class TextLogger
+    {
+        private readonly string _path;
+
+        public TextLogger(string path)
+        {
+            _path = path;
+        }
+
+        public void WriteLine(string text)
+        {
+            using (var stream = File.AppendText(_path))
+            {
+                stream.WriteLine(text);
+            }
+        }
+    }
+}
